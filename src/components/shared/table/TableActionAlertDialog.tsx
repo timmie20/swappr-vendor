9ff2d44ee2a7c714ec7a1265a135f6871ccccd6d
whoshnri <henrybassey2007@gmail.com>
2026-02-27 17:20:09 +0100

    fix: delete alert-modal button fix

--- src/components/shared/table/TableActionAlertDialog.tsx
+++ src/components/shared/table/TableActionAlertDialog.tsx
@@ -12,8 +12,11 @@ import {
 } from "@/components/ui/alert-dialog";
 import { toast } from "sonner";
 import { AlertDialogTooltip } from "@/components/shared/table/TableActionTooltip";
-import { FormSubmitButton } from "../form/SubmitButton";
+import { SubmitButton } from "../form/SubmitButton";
 import { ServerActionResponse } from "@/types/server-action";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Loader, Loader2 } from "lucide-react";
 
 type Props = {
   title: string;
@@ -68,14 +71,17 @@ export function TableActionAlertDialog({
 
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <FormSubmitButton
-            variant="destructive"
-            isPending={isPending}
+          <Button
+            disabled={isPending}
             onClick={handleConfirm}
-            className="px-4 py-2"
+            type="submit"
+            // className={cn("px-4 py-2")}
+            size="lg"
+            variant="destructive"
           >
+            {isPending && <Loader2 className="mr-2 size-4 animate-spin" />}
             {actionButtonText}
-          </FormSubmitButton>
+          </Button>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
